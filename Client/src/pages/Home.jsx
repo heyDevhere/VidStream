9ba1c443c9d47e0d153228ffe77c9bd61661b987
heyDevhere <mdev3888@gmail.com>
2024-06-04 00:37:56 +0530

    Vid

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -9,62 +9,25 @@ const Container = styled.div`
   flex-wrap: wrap;
 `;
 
-const flattenData = (data) => {
-  const result = [];
-  
-  const flatten = (item) => {
-    if (Array.isArray(item)) {
-      item.forEach(flatten);
-    } else if (item && typeof item === 'object') {
-      result.push(item);
-    }
-  };
-
-  flatten(data);
-  return result;
-};
 
 const Home = ({type}) => {
   const [videos, setVideos] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchVideos = async () => {
-  //     const res = await axios.get(`/videos/${type}`);
-  //     console.log(res.data);
-  //     setVideos(res.data);
-  //   };
-  //   fetchVideos();
-  // }, [type]);
-
-
   useEffect(() => {
     const fetchVideos = async () => {
-      try {
-        const res = await axios.get(`/videos/${type}`);
-        console.log('API Response:', res.data); // Log the response data to check its structure
-
-        if (Array.isArray(res.data) || (res.data && typeof res.data === 'object')) {
-          const flattenedData = flattenData(res.data);
-          setVideos(flattenedData);
-        } else {
-          console.error("Response is not an array or object", res.data);
-          setVideos([]);
-        }
-      } catch (error) {
-        console.error("Error fetching videos:", error);
-        setVideos([]);
-      }
+      const res = await axios.get(`/videos/${type}`);
+      console.log(res.data);
+      setVideos(res.data);
     };
     fetchVideos();
   }, [type]);
 
-
   return (
     <Container>
-      {/* {videos.map((video) => (
+      {videos.map((video) => (
 
         <Card key={video._id} video={video}/>
-      ))} */}
+      ))}
     </Container>
   );
 };
